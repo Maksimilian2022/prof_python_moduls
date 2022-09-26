c51a4c6e7fe7005d8a89ea753f601ca9a58f504f
Maksimilian Romanov <maks.romanov.15@inbox.ru>
2022-09-26 21:59:59 +0300

    add modul

---
+++ application/db/people.py
@@ -0,0 +1,5 @@
+def get_employees():
+    print("Какие-то сотруднии")
+    return
+if __name__ == '__people__':
+    get_employees()
---
+++ application/salary.py
@@ -0,0 +1,5 @@
+def calculate_salary():
+    print("Какая-то посчитанная информация")
+    return
+if __name__ == '__salary__':
+    calculate_salary()
